8de090a56c9eef9f65a08a1b3cc87991fe86c3ab
bducduy <ducduy993@gmail.com>
2021-09-21 21:11:22 +0700

    change ij code

--- js/1.js
+++ js/1.js
@@ -4,12 +4,14 @@ $(document).ready(function() {
 $(window).scroll(function (event) {
 var st = $(this).scrollTop();
 var ss = $(".about").offset().top;
-if ((st >= 0) && (st < ss)) {
-    $('.dautrang img').animate({top: st/3}, 0);
-};
-if (st > ss) {
-    $('.dautrang img').animate({top: st/3}, 0);
-}
+    if (st < ss) {
+        $('.dautrang img').animate({top: st/3}, 0);
+    }
+    else {
+        $('.dautrang img').animate({top: ss/3}, 0);
+    }
+
+
 
 });
 });
