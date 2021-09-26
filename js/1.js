987560d62e12c8d4a47ca55b0d0bcb120f98bd49
bducduy <ducduy993@gmail.com>
2021-09-27 02:19:38 +0700

    typing carousel effect

--- js/1.js
+++ js/1.js
@@ -89,7 +89,7 @@ else {
 
 /*counter numbers*/
 
-if (st > ($('.counter').offset().top)-200) {
+if (st > ($('.counter').offset().top)-300) {
 
     $('.counter .number').each(function () {
         var per = $(this).attr('per');
@@ -236,7 +236,63 @@ $('.doitac .owl-carousel').owlCarousel({
         }
     }
 })
+/********************************/
 
+/*typing carousel effect*/
+const carouselText = [
+  {text: "Web Developer.", color: "white"},
+  {text: "Web Designer.", color: "white"},
+  {text: "Graphic Designer.", color: "white"},
+  {text: "Web Developer.", color: "white"}
+]
 
+$( document ).ready(async function() {
+  carousel(carouselText, "#feature-text")
+});
+
+async function typeSentence(sentence, eleRef, delay = 100) {
+  const letters = sentence.split("");
+  let i = 0;
+  while(i < letters.length) {
+    await waitForMs(delay);
+    $(eleRef).append(letters[i]);
+    i++
+  }
+  return;
+}
+
+async function deleteSentence(eleRef) {
+  const sentence = $(eleRef).html();
+  const letters = sentence.split("");
+  let i = 0;
+  while(letters.length > 0) {
+    await waitForMs(100);
+    letters.pop();
+    $(eleRef).html(letters.join(""));
+  }
+}
+
+async function carousel(carouselList, eleRef) {
+    var i = 0;
+    while(true) {
+      updateFontColor(eleRef, carouselList[i].color)
+      await typeSentence(carouselList[i].text, eleRef);
+      await waitForMs(1500);
+      await deleteSentence(eleRef);
+      await waitForMs(500);
+      i++
+      if(i >= carouselList.length) {i = 0;}
+    }
+}
+
+function updateFontColor(eleRef, color) {
+  $(eleRef).css('color', color);
+}
+
+function waitForMs(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+/*****************/
 
 });
